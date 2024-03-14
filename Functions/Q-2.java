608996e53731ddc28922f182cd2cd20ae5739a29
niteshjain96 <niteshjain5432@gmail.com>
2024-03-14 23:17:34 +0530

    functions

---
+++ Functions/Q-2.java
@@ -0,0 +1,26 @@
+// Decimal to any Base
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int b=scn.nextInt();
+        int dn=getValueInBase(n,b);
+        System.out.print(dn);
+    }
+    
+    public static int getValueInBase(int n,int b){
+        int dn=0;
+        int tp=1;
+        while(n>0){
+            int r=n%b;
+            n=n/b;
+            
+            dn=dn+r*tp;
+            tp=tp*10;
+        }
+        return dn;
+    }
+}
