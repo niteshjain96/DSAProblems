391372a72272a10002ba885fef53aa894d080863
niteshjain96 <niteshjain5432@gmail.com>
2024-04-17 23:20:39 +0530

    java

--- Recursion On The Way Up/Q-1.java
+++ Recursion On The Way Up/Q-1.java
@@ -1,15 +1,11 @@
 // Print Subsequence
-
 import java.util.*;
-
 public class Main{
     public static void main(String[] args){
         Scanner scn = new Scanner(System.in);
         String str=scn.nextLine();
-
         printSS(str,"");
     }
-
     public static void printSS(String ques,String ans){
         if(ques.length()==0){
             System.out.println(ans);
@@ -17,7 +13,6 @@ public class Main{
         }
         char ch=ques.charAt(0);
         String roq=ques.substring(1);
-
         printSS(roq,ans+ ch);
         printSS(roq,ans+"");
     }
