085970a062895fcd410184c3047a4e07745e384a
niteshjain96 <niteshjain5432@gmail.com>
2024-04-18 17:44:34 +0530

    java

---
+++ Time Complexity/forfor.java
@@ -0,0 +1,9 @@
+// 
+for(int i=1;i<=n;i+=c){
+    for(int j=1;j<=c;j++){
+        // do some
+        // T(n)= n/c  * c * k = nK;
+    }
+}
+
+//
---
+++ Time Complexity/text.java
@@ -0,0 +1,15 @@
+for(int i=1;i<=n;i++){
+    // do some
+    // T(n)= k+T(n-1)= kn;
+}
+
+for(int i=n;i>=1;i--){
+    // do some
+    // T(n)= k+T(n-1)= kn
+}
+
+for(int i=n;i>=1;i=i/2){
+    // do some
+    // T(n)= klog(n);
+}
+
