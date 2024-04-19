9bf1c1a399f8f58986218f2cd69c85315a8d3569
niteshjain96 <niteshjain5432@gmail.com>
2024-04-19 23:30:41 +0530

    java

---
+++ Time Complexity/for5.java
@@ -0,0 +1,12 @@
+//
+
+for(int i=1,j=1;i<=n;j++){
+    // do some
+    if(j>i){
+        j=1;
+        i++;
+    }
+}
+
+// T(n) directly proportional to n^2;
+
---
+++ Time Complexity/polynomial.java
@@ -0,0 +1,21 @@
+// polynomial in O(n)
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scn= new Scanner(System.in);
+        int x=scn.nextInt();
+        int n=scn.nextInt();
+        int c=n;
+        int pox=x;
+        int ans=0;
+        while(c>=1){
+            int term=c * (pox);
+            ans +=term;
+            c--;
+            pox=pox*x;
+        }
+        System.out.println(ans);
+    }
+}
